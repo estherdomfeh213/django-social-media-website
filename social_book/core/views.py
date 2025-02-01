0a8a1e324c3685844834e99e4c6ea62744c31307
estherdomfeh213 <domfehesther2020@gmail.com>
2025-02-01 11:49:02 +0000

    configure signin view

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
@@ -29,11 +30,10 @@ def signup(request):
             user = User.objects.create_user(username=username, email=email, password=password)
             user.save()
             
-            # log user in and redirect to settins page
+            #! log user in and redirect to settings page
             
              
             # Creaet a Profile object for the new user
-            # 
             user_model = User.objects.get(username=username)
             new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
             new_profile.save()
@@ -46,3 +46,21 @@ def signup(request):
      
    else:
       return render(request, 'signup.html')
+   
+   
+def signin(request):
+   
+   if request.method == 'POST':
+      username = request.POST['username']
+      password = request.POST['password']
+      
+      user = auth.authenticate(username=username, password=password)
+      
+      if user is not None:
+         auth.login(request, user)
+         return redirect('/')
+      else:
+         messages.info(request, 'Credentials Invalid')
+         return redirect('signin')
+   else:
+      return render(request, 'signin.html')
