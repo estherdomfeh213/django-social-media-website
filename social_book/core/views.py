e8df21a31a743b213eeee50718cafbba897617d5
estherdomfeh213 <domfehesther2020@gmail.com>
2025-02-01 10:51:03 +0000

    update profile view & created two users for trails

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
   
 
 # Create your views here.
@@ -27,6 +28,16 @@ def signup(request):
          else:
             user = User.objects.create_user(username=username, email=email, password=password)
             user.save()
+            
+            # log user in and redirect to settins page
+            
+             
+            # Creaet a Profile object for the new user
+            # 
+            user_model = User.objects.get(username=username)
+            new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+            new_profile.save()
+            return redirect('signup')
       
       else:
          messages.info(request, 'Password Not Martching')
