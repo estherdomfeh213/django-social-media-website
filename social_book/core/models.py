5c5dc705e60b7bd70a3ba3061de80a2574ea43f7
estherdomfeh213 <domfehesther2020@gmail.com>
2025-02-10 20:08:15 +0000

    add likepost model

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -23,7 +23,13 @@ class Profile(models.Model):
     
     def __str__(self):
       return self.user
+  
+  class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
     
+    def __str__(self):
+      return self.username
     
     
     
